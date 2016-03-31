3e52f89405ac46e9bb75ccd8b313e4c8a143e5ff
jacobmumm <jacob.mumm@gmail.com>
2016-03-31 12:48:05 -0400

    managing interval with subjects

--- app.js
+++ app.js
@@ -9,18 +9,13 @@ app.controller('Ctrl', function Ctrl($scope, $http, DsPoller){
 
   poller.setAction(function () { return $http.get('http://localhost:8888/cases')});
 
-  poller.setHandler(function(res) {
+  poller.subscribe(function(res) {
     $scope.$apply(function(){
-      $scope.items = res;
+      $scope.items = res.data;
     });
   });
   
   poller.start();
-  window.poller = poller;
-  /*setTimeout(function(){
-    // not really working yet
-    poller.stop();
-  }, 20000);*/
 
 });
 
@@ -36,67 +31,60 @@ app.provider('DsPoller', function() {
 
         function DsPoller (group, config) {
           this.group = group;
-          pollers[group] = this;
-
-         
-          this.setConfig(config);
-          
+          pollers[group] = this;       
           this.action = function(){}
           this.handler = function(){}
+          this.errorCount$ = new rx.BehaviorSubject(0)
+          this.interval$ = new rx.BehaviorSubject(0);
+          this.maxInterval$ = new rx.BehaviorSubject(0);
+                   
+          this.computedInterval$ = this.interval$
+            .zip(this.errorCount$, this.maxInterval$, function(interval, errorCnt, maxInt){
+              var calcInt = interval * Math.pow(2, errorCnt);
+              return Math.min(calcInt, maxInt);
+            });
           
           var _this = this;
-
-          function computeInterval(error) {
-            if (error) {
-              _this.interval = _this.interval < _this.maxInterval ? _this.interval * 2 : _this.maxInterval;
-            } else {
-              _this.interval = _this.initinterval;
-            }
-            return _this.interval;
-          }
-          
           this.poller$ = rx.Observable.fromPromise(function(){ return _this.action(); })
-            .retryWhen(function(errors){
-              return errors.scan(function(acc, x) { return acc + x; }, 0)
-                .flatMap(function(x){ 
-                  return rx.Observable.timer(computeInterval(true));
-                });
-            })
             .repeatWhen(function(notification){
               return notification
-                .scan(function(acc, x) { return acc + x; }, 0)
-                .flatMap(function(x){ 
-                  return rx.Observable.timer(computeInterval());
-                });
-            });
+                .do(function(){ return _this.errorCount$.onNext(0); })
+                .flatMap(function(){ return _this.computedInterval$; })
+                .flatMap(function(interval){ return rx.Observable.timer(interval); });
+            })
+            .retryWhen(function(errors){
+              return errors
+                .do(function(err) { return _this.errorCount$.onNext(_this.errorCount$.getValue() + 1); })
+                .flatMap(function() { return _this.computedInterval$; })
+                .flatMap(function(interval) { return rx.Observable.timer(interval); });
+            })
+            .publish();
+
+            this.setConfig(config); 
         }
       
         DsPoller.prototype.setConfig = function(config) {
-          this.setInterval(config && config.interval || 5000);
-          this.maxInterval = config && config.maxInterval || 300000;
-        }
-        
-        DsPoller.prototype.setInterval = function(int) {
-          this.interval = this.initinterval = int;
+          this.interval$.onNext(config && config.interval || this.interval$.getValue() || 5000);
+          this.maxInterval$.onNext(config && config.maxInterval || this.maxInterval$.getValue() || 300000);
         }
         
         DsPoller.prototype.setAction = function(action) {
           this.action = action;
         }
-
-        DsPoller.prototype.setHandler = function(handler) {
-          this.handler = handler;
+        
+        DsPoller.prototype.subscribe = function(cb) {
+          this.poller$.subscribe(cb);
         }
         
         DsPoller.prototype.start = function(forceStart) {
           var _this = this;
-          rx.Observable.timer(forceStart ? 0 : this.interval).subscribe(function(){
-            _this.unsubscribe$ = _this.poller$.subscribe(this.handler);
+          rx.Observable.timer(forceStart ? 0 : this.interval$.getValue()).subscribe(function(){
+            _this.connection = _this.poller$.connect();
           });
         }
         
         DsPoller.prototype.stop = function() {
-          this.unsubscribe$.dispose();
+          this.connection.dispose();
         }
         
         DsPoller.prototype.destroy = function() {
