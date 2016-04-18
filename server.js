4ec57af566b80407856d3522ddaaadb3cb8dbc27
Jason Aden <jasonaden@gmail.com>
2016-04-18 13:14:58 -0700

    Fix cross-origin request.

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var cases = require('./data/cases');
-var http = require('http');
 var httpProxy = require('http-proxy');
 var config = require('./config.json');
 
@@ -22,41 +20,12 @@ var proxy = httpProxy.createProxyServer({
   auth: config.username + ':' + config.password
 });
 
-var server = http.createServer(function (req, res) {
+app.get('/api/*', function (req, res) {
   proxy.web(req, res, {
     target: config.server
   })
+})
+
+app.listen(8888, function () {
+  console.log('Example app listening on 8888!');
 });
-
-console.log('Proxy listening on 8888!');
-server.listen(8888)
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.get('/cases', function(req, res) {
-//   var action = Math.ceil(Math.random()*10);
-//   var count = Math.floor(Math.random()*10);
-
-//   res.json(cases);
-//   // if (action <= 4 && false) {
-//   //   res.sendStatus(500);
-//   // } else if (false && (action == 5 || action == 6)) {
-//   //   setTimeout(function () {
-//   //     if (action == 5) {
-//   //       res.sendStatus(500);
-//   //     } else {
-//   //       res.json(cases.slice(count));
-//   //     }
-//   //   }, 3000)
-//   // } else {
-//   //   res.json(cases.slice(count));
-//   // }
-// });
-
-
-
-// app.listen(8888, function () {
-//   console.log('Example app listening on 8888!');
-// });
